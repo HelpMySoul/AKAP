537ac2cc218322e288aea9d499e8eabacd7d506a
HelpMySoul <shalamaev.es.hms@gmail.com>
2025-06-04 09:52:43 +0300

    rollback

--- app/src/main/kotlin/player/managers/SettingCardManager.kt
+++
@@ -1,9 +0,0 @@
-package playlistMenu.managers
-
-import playlistMenu.interfaces.ISong
-
-object SettingCardManager {
-    fun showSettingCard(song: ISong): (ISong) -> Unit {
-        return TODO()
-    }
-}
